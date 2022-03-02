8eb4cb1912cc9842a67cb179132f928b534bbf73
ROHHYEONMIN <roh._.hm@RohHyeonMinui-MacBookPro.local>
2022-03-02 11:07:07 +0900

    Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,100 +1,24 @@
-/*export const people = [
-    {
-        id: 0,
-        name: "Nicolas",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id: 1,
-        name: "Jisu",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id: 2,
-        name: "Japan Guy",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: 3,
-        name: "Daal",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: 4,
-        name: "JD",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: 5,
-        name: "moondaddi",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: 6,
-        name: "flynn",
-        age: 18,
-        gender: "male"
+import fetch from "cross-fetch";
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
+const DETAIL_URL = "https://yts.mx/api/v2/movie_details.json?";
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+
+    if (limit > 0) {
+        REQUEST_URL += `limit=${limit}`
     }
-];
-
-export const getById = id => {
-    const filteredPeople = people.filter(p => id === p.id);
-    return filteredPeople[0];
-}*/
-
-let movies = [
-    {
-        id: 0,
-        name: "Star Wars - The new one",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Avengers - The new one",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "The Godfather I",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "Logan",
-        score: 2
+    if (rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
-];
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
+    return fetch(REQUEST_URL)
+        .then(res => res.json())
+        .then(json => json.data.movies);
 };
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score,
-    }
-
-    movies.push(newMovie);
-    return newMovie;
-};
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
+export const getMovie = (id) => {
+    return fetch(`${DETAIL_URL}movie_id=${id}`)
+        .then(res => res.json())
+        .then(json => json.data.movie);
 };
